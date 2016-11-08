5cbd5a961271da3693dd538c5c9fcaf459f2d937
Metin Bulus <contact@metinbulus.com>
2016-11-08 11:59:59 -0600

    Update obj.conv.R

--- R/obj.conv.R
+++ R/obj.conv.R
@@ -5,7 +5,8 @@ optimal.to.mdes <- function(design){
   for(i in 1:length(idx.par[!is.na(idx.par)])){
     design$par[idx.par[!is.na(idx.par)][i]] <- design$round.optim[idx.out[!is.na(idx.out)][i]]
   }
-  design$fun <- paste0("mdes", substr(design$fun,8,14))
+  design$fun <- paste0("mdes", sub('.*\\.', '', design$fun))
+  # design$fun <- paste0("mdes", substr(design$fun,8,14))
   return(do.call(design$fun,design$par))
 }
 optimal.to.power <- function(design){
@@ -14,14 +15,17 @@ optimal.to.power <- function(design){
   for(i in 1:length(idx.par[!is.na(idx.par)])){
     design$par[idx.par[!is.na(idx.par)][i]] <- design$round.optim[idx.out[!is.na(idx.out)][i]]
   }
-  design$fun <- paste0("power", substr(design$fun,8,14))
+  design$fun <- paste0("power", sub('.*\\.', '', design$fun))
+  #design$fun <- paste0("power", substr(design$fun,8,14))
   return(do.call(design$fun,design$par))
 }
 mrss.to.mdes <- function(design){
-  design$fun <- paste0("mdes", substr(design$fun,5,11))
+  design$fun <- paste0("mdes", sub('.*\\.', '', design$fun))
+  #design$fun <- paste0("mdes", substr(design$fun,5,11))
   return(do.call(design$fun,design$par))
 }
 mrss.to.power <- function(design){
-  design$fun <- paste0("power", substr(design$fun,5,11))
+  design$fun <- paste0("power", sub('.*\\.', '', design$fun))
+  #design$fun <- paste0("power", substr(design$fun,5,11))
   return(do.call(design$fun,design$par))
 }
